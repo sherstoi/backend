1031804c8987ef5a7a4f66c6e0ec5c03b7d44b5f
sherstoi <sherstoi83@gmail.com>
2017-09-28 11:12:02 +0200

    update context.xml

--- statistics/src/main/java/com/britebill/interview/statistics/beans/Statistics.java
+++ statistics/src/main/java/com/britebill/interview/statistics/beans/Statistics.java
@@ -1,5 +1,40 @@
 package com.britebill.interview.statistics.beans;
 
 public class Statistics {
+    private String mostRepeatedWord;
+    private Long totalNumberOfWords;
+    private Long totalNumberOfUniqueWords;
+    private Long averageCharactersPerWord;
 
+    public String getMostRepeatedWord() {
+        return mostRepeatedWord;
+    }
+
+    public void setMostRepeatedWord(String mostRepeatedWord) {
+        this.mostRepeatedWord = mostRepeatedWord;
+    }
+
+    public Long getTotalNumberOfWords() {
+        return totalNumberOfWords;
+    }
+
+    public void setTotalNumberOfWords(Long totalNumberOfWords) {
+        this.totalNumberOfWords = totalNumberOfWords;
+    }
+
+    public Long getTotalNumberOfUniqueWords() {
+        return totalNumberOfUniqueWords;
+    }
+
+    public void setTotalNumberOfUniqueWords(Long totalNumberOfUniqueWords) {
+        this.totalNumberOfUniqueWords = totalNumberOfUniqueWords;
+    }
+
+    public Long getAverageCharactersPerWord() {
+        return averageCharactersPerWord;
+    }
+
+    public void setAverageCharactersPerWord(Long averageCharactersPerWord) {
+        this.averageCharactersPerWord = averageCharactersPerWord;
+    }
 }
