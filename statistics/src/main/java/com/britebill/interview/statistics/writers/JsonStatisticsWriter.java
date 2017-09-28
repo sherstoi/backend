0d97d9cd2ee4822153a7f58f83a2f8fa688241f6
sherstoi <sherstoi83@gmail.com>
2017-09-28 12:13:19 +0200

    update JAXB formatter

--- statistics/src/main/java/com/britebill/interview/statistics/writers/JsonStatisticsWriter.java
+++ statistics/src/main/java/com/britebill/interview/statistics/writers/JsonStatisticsWriter.java
@@ -1,13 +1,27 @@
 package com.britebill.interview.statistics.writers;
 
 import com.britebill.interview.statistics.beans.Statistics;
+import org.eclipse.persistence.jaxb.JAXBContextProperties;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class JsonStatisticsWriter implements StatisticsWriter {
 
     public void write(Statistics statistics, File file) {
-        // TODO: Add your JSON writer implementation here
+        try {
+            JAXBContext jc = org.eclipse.persistence.jaxb.JAXBContextFactory
+                    .createContext(new Class[] { com.britebill.interview.statistics.beans.Statistics.class }, null);
+            Marshaller marsh = jc.createMarshaller();
+            marsh.setProperty("eclipselink.media-type", "application/json");
+            marsh.marshal(statistics, file);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
     }
 }
