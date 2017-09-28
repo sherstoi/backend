8cd0bb4f0fa45ee110df716601c77300a537fb75
sherstoi <sherstoi83@gmail.com>
2017-09-28 12:46:10 +0200

    update JAXB formatter

--- statistics/src/main/java/com/britebill/interview/statistics/writers/XmlStatisticsWriter.java
+++ statistics/src/main/java/com/britebill/interview/statistics/writers/XmlStatisticsWriter.java
@@ -14,12 +14,10 @@ public class XmlStatisticsWriter implements StatisticsWriter {
         try {
             JAXBContext jaxbContext = JAXBContext.newInstance(Statistics.class);
             Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-            // output pretty printed
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
             jaxbMarshaller.marshal(statistics, file);
         } catch (JAXBException j) {
             j.printStackTrace();
         }
-        // TODO: Add your XML writer implementation here
     }
 }
--- transformers/src/main/java/com/britebill/interview/transformers/TransformerData.java
+++ transformers/src/main/java/com/britebill/interview/transformers/TransformerData.java
@@ -1,10 +1,32 @@
 package com.britebill.interview.transformers;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TransformerData {
     public List<String> transformData(List<String> src) {
-        // TODO: Add here your code
-        return null;
+        List<String> uniqueWordList = withoutRepeatedWords(src);
+        List<String> sortedList = sortedByLength(src);
+        List<String> upCaseList = convertToUpperCaseList(src);
+
+        return Stream.concat(Stream.concat(uniqueWordList.stream(), sortedList.stream()),
+                upCaseList.stream()).collect(Collectors.toList());
+    }
+
+    private List<String> withoutRepeatedWords(List<String> stringList) {
+        return new ArrayList<>(stringList.stream().distinct().collect(Collectors.toList()));
+    }
+
+    private List<String> sortedByLength(List<String> stringList) {
+        List<String> sortedList = new ArrayList<>(stringList);
+        sortedList.sort(Comparator.comparing(String::length));
+        return sortedList;
+    }
+
+    private List<String> convertToUpperCaseList(List<String> stringList) {
+        return new ArrayList<>(stringList.stream().map(String::toUpperCase).collect(Collectors.toList()));
     }
 }
